20c42ee4b8be68e83ec12894d62584ff3203d18f
Aman Singh <smohan35895@gmail.com>
2024-08-02 21:53:31 +0530

    add the anchor tag to the userimage on the header component

--- src/components/RiversideParkDevelopment/index.jsx
+++ src/components/RiversideParkDevelopment/index.jsx
@@ -10,10 +10,19 @@ export default function RiversideParkDevelopment({
   ...props
 }) {
   return (
-    <div {...props} className={ `${props.className} h-[590px] w-full md:h-auto relative`}>
-      <Img src={userImage} alt="Image" className="mx-auto h-[590px] w-full flex-1 object-cover" />
-      <div className="absolute bottom-0 left-0 right-0 top-0 m-auto h-max flex-1 bg-blue_gray-800_b2
-      bg-blend-lighten px-10 py-[115px] md:py-5 sm:p-5">
+    <div
+      {...props}
+      className={`${props.className} h-[590px] w-full md:h-auto relative`}
+    >
+      <Img
+        src={userImage}
+        alt="Image"
+        className="mx-auto h-[590px] w-full flex-1 object-cover"
+      />
+      <div
+        className="absolute bottom-0 left-0 right-0 top-0 m-auto h-max flex-1 bg-blue_gray-800_b2
+      bg-blend-lighten px-10 py-[115px] md:py-5 sm:p-5"
+      >
         <div className="mt-16 flex flex-col gap-4 sm:gap-4">
           <Heading
             size="heading2x1"
@@ -23,12 +32,21 @@ export default function RiversideParkDevelopment({
             {title}
           </Heading>
           <div className="flex flex-col items-start">
-            <Text as="p" className="w-full leading-[26px] sm:w-full sm:text-[13px]">
+            <Text
+              as="p"
+              className="w-full leading-[26px] sm:w-full sm:text-[13px]"
+            >
               {description}
             </Text>
-            <Button color="deep_orange_A700" shape="square" className="relative mt-[12px] min-w-[150px] font-bold">
-              {seeMoreButton}
-            </Button>
+            <a href="/moreinfopageforaprojectprev">
+              <Button
+                color="deep_orange_A700"
+                shape="square"
+                className="relative mt-[12px] min-w-[150px] font-bold"
+              >
+                {seeMoreButton}
+              </Button>
+            </a>
           </div>
         </div>
       </div>
